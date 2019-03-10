be0ac98d3fe27a2623fb7174b5ee8ce29440c41e
Eduardo Pereira <pereira.somoza@gmail.com>
2019-03-10 17:38:27 -0300

    Criando repositório particular para a lib

---
+++ Legs.cpp
@@ -0,0 +1,407 @@
+/*
+Copyright 2019 Eduardo S. Pereira
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "Legs.h"
+#include <Wire.h>
+
+Legs * Legs::legsi2c = NULL;
+
+Legs::Legs(){
+    /*
+    Constructor fo classe.
+    Start legsi2c as the current object.
+    */
+    legsi2c = this;
+}
+
+states Legs::get_current_state(){
+    /*
+    Return the current moviment state.
+    States enumerator order:
+        FORWARD
+        BACKWARD
+        LEFT
+        RIGHT
+        BYELEFT
+        BYERIGHT
+        STOP
+        ENDSTATE
+    */
+    return current_state;
+}
+
+void Legs::set_servos(int rs1, int ri1,
+          int rs2, int ri2,
+          int ls1, int li1,
+          int ls2, int li2){
+    /*
+    set servos ports.
+    Parameters:
+        rs1: integer port connected to right front superior servo.
+        ri1: integer port connected to right front inferior servo.
+        rs2: integer port connected to right back superior servo.
+        ri2: integer port connected to right back inferior servo.
+        ls1: integer port connected to left front superior servo.
+        li1: integer port connected to left front inferior servo.
+        ls2: integer port connected to left back superior servo.
+        li2: integer port connected to left back inferior servo.
+
+                         Front
+                     ---------------
+                    |     O   O     |
+                    |---------------|
+                    |               |
+              ----- ---------------  -----
+             | RS1 |                | LS1 |
+              ----                    ----
+         -----                              -----
+        | RI1 |                            | LI1 |
+         -----                              -----
+
+                        Back
+                   ---------------
+                  |               |
+                  |---------------|
+                  |               |
+            ----- ---------------  -----
+            | LS2 |                | RS2 |
+             ----                    ----
+         -----                           -----
+        | LI2 |                          | RI2 |
+         -----                            -----
+    */
+
+    servo_rs1.attach(rs1);
+    servo_ri1.attach(ri1);
+    servo_rs2.attach(rs2);
+    servo_ri2.attach(ri2);
+    servo_ls1.attach(ls1);
+    servo_li1.attach(li1);
+    servo_ls2.attach(ls2);
+    servo_li2.attach(li2);
+
+}
+
+void Legs::start_i2c(){
+    /*
+    Start i2c communication.
+    */
+    Wire.begin(i2c_address);
+    Wire.onReceive(this->receive_data);
+    Wire.onRequest(this->send_data);
+}
+
+void Legs::receive_data(int byte_count){
+    /*
+    Receive data from i2c communication.
+    Parameter:
+        byte_count: integer;
+    */
+
+    while(Wire.available()){
+        int read = Wire.read();
+
+        switch(read){
+            case 0:
+            legsi2c->set_current_state(FORWARD);
+            break;
+            case 1:
+            legsi2c->set_current_state(BACKWARD);
+            break;
+            case 2:
+            legsi2c->set_current_state(LEFT);
+            break;
+            case 3:
+            legsi2c->set_current_state(RIGHT);
+            break;
+            case 4:
+            legsi2c->set_current_state(BYELEFT);
+            break;
+            case 5:
+            legsi2c->set_current_state(BYERIGHT);
+            break;
+            case 6:
+            legsi2c->set_current_state(STOP);
+            break;
+            default:
+            legsi2c->set_current_state(STOP);
+            break;
+        }
+    }
+}
+
+void Legs::send_data(){
+    /*
+    Send data to i2c communication.
+    Send the current legs moviment state.
+    Info total bytes: 32
+    */
+    String info = "s:" +
+                  String(legsi2c->get_current_state()) +
+                  ",ls:" +
+                  String(legsi2c->is_last_step_movimet()) +
+                  ",v:" +
+                  String(legsi2c->get_velocity(), 3) +
+                  ",d:" +
+                  String(legsi2c->get_distance(), 3);
+
+    Serial.println(info);
+
+    Wire.write(info.c_str());
+
+}
+
+void Legs::zero_pos(){
+    /*
+    Put all servos in initial position.
+    */
+    //  rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+    step(180, 0, 0, 180, 0, 180, 180, 0);
+}
+
+void Legs::step(int rs1, int  rs2, int  ls1, int  ls2,
+                int ri1, int  ri2, int  li1, int  li2){
+    /*
+    Move all servos to a defined angle:
+    Parameters:
+        rs1: integer angle of right front superior servo.
+        ri1: integer angle of right front inferior servo.
+        rs2: integer angle of right back superior servo.
+        ri2: integer angle of right back inferior servo.
+        ls1: integer angle of left front superior servo.
+        li1: integer angle of left front inferior servo.
+        ls2: integer pngle of left back superior servo.
+        li2: integer angle of left back inferior servo.
+
+    */
+    servo_rs1.write(rs1);
+    servo_rs2.write(rs2);
+    servo_ls1.write(ls1);
+    servo_ls2.write(ls2);
+
+    servo_ri1.write(ri1);
+    servo_ri2.write(ri2);
+    servo_li1.write(li1);
+    servo_li2.write(li2);
+
+}
+
+void Legs::move_forward(){
+    /*
+    Move robot forward.
+    */
+    if(cont_back_for > 13){
+        cont_back_for = 0;
+        last_step_movimet = true;
+    }else{
+        last_step_movimet = false;
+    }
+
+    if(millis() - t0 >= _delay_step){
+        t0 = millis();
+        // rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+        step((int) _foward[cont_back_for][0],(int) _foward[cont_back_for][1],
+             (int)_foward[cont_back_for][2],(int) _foward[cont_back_for][3],
+             (int)_foward[cont_back_for][4],(int) _foward[cont_back_for][5],
+             (int) _foward[cont_back_for][6],(int) _foward[cont_back_for][7]);
+        cont_back_for += 1;
+    }
+}
+
+void Legs::move_backward(){
+    /*
+    Move robot backward.
+    */
+
+    if(cont_back_for > 14){
+        cont_back_for = 0;
+        last_step_movimet = true;
+    }else{
+        last_step_movimet = false;
+    }
+
+    if(millis() - t0 >= _delay_step){
+        t0 = millis();
+        // rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+        step((int)_backward[cont_back_for][0],(int) _backward[cont_back_for][1],
+             (int) _backward[cont_back_for][2],(int) _backward[cont_back_for][3],
+             (int) _backward[cont_back_for][4],(int) _backward[cont_back_for][5],
+             (int) _backward[cont_back_for][6], (int)_backward[cont_back_for][7]);
+        cont_back_for += 1;
+    }
+}
+
+
+void Legs::turn_right(){
+    /*
+    Turn robot to right direction.
+    */
+
+    if(cont_left_right > 3){
+        cont_back_for = 0;
+        last_step_movimet = true;
+    }else{
+        last_step_movimet = false;
+    }
+
+    if(millis() - t0 >= _delay_turn){
+        t0 = millis();
+        // rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+        step((int)_right[cont_left_right][0],(int) _right[cont_left_right][1],
+             (int)_right[cont_left_right][2],(int) _right[cont_left_right][3],
+             (int)_right[cont_left_right][4],(int) _right[cont_left_right][5],
+             (int) _right[cont_left_right][6],(int) _right[cont_left_right][7]);
+        cont_left_right += 1;
+    }
+}
+
+
+void Legs::turn_left(){
+    /*
+    Turn robot to the left direction.
+    */
+
+    if(cont_left_right > 3){
+        cont_left_right = 0;
+        last_step_movimet = true;
+    }else{
+        last_step_movimet = false;
+    }
+
+    if(millis() - t0 >= _delay_turn){
+        t0 = millis();
+        // rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+        step((int)_left[cont_left_right][0], (int) _left[cont_left_right][1],
+             (int) _left[cont_left_right][2],(int) _left[cont_left_right][3],
+             (int) _left[cont_left_right][4],(int) _left[cont_left_right][5],
+             (int) _left[cont_left_right][6],(int) _left[cont_left_right][7]);
+        cont_left_right += 1;
+    }
+}
+
+void Legs::bye_bye_left(){
+    /*
+    Send bye with left front leg.
+    */
+
+    if(cont_bye > 6){
+        cont_bye = 0;
+        last_step_movimet = true;
+    }else{
+        last_step_movimet = false;
+    }
+
+    if(millis() - t0 >= _delay_bye){
+        t0 = millis();
+        // rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+        step((int)_bye_left[cont_bye][0], (int) _bye_left[cont_bye][1],
+             (int) _bye_left[cont_bye][2], (int) _bye_left[cont_bye][3],
+             (int) _bye_left[cont_bye][4],(int) _bye_left[cont_bye][5],
+             (int)_bye_left[cont_bye][6],(int) _bye_left[cont_bye][7]);
+        cont_bye += 1;
+    }
+
+}
+
+void Legs::bye_bye_right(){
+    /*
+    Send bye with right front leg.
+    */
+
+    if(cont_bye > 6){
+        cont_bye = 0;
+        last_step_movimet = true;
+    }else{
+        last_step_movimet = false;
+    }
+
+    if(millis() - t0 >= _delay_bye){
+        t0 = millis();
+        // rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+        step((int)_bye_right[cont_bye][0], (int)_bye_right[cont_bye][1],
+             (int) _bye_right[cont_bye][2],(int) _bye_right[cont_bye][3],
+             (int)_bye_right[cont_bye][4], (int)_bye_right[cont_bye][5],
+             (int) _bye_right[cont_bye][6], (int)_bye_right[cont_bye][7]);
+        cont_bye += 1;
+    }
+
+}
+
+
+void Legs::move_according_state(){
+    /*
+    Given the current state, update the moviment.
+    */
+    switch (get_current_state()) {
+        case FORWARD:
+        move_forward();
+        break;
+        case BACKWARD:
+        move_backward();
+        break;
+        case LEFT:
+        turn_left();
+        break;
+        case RIGHT:
+        turn_right();
+        break;
+        case BYELEFT:
+        bye_bye_left();
+        break;
+        case BYERIGHT:
+        bye_bye_right();
+        break;
+        case STOP:
+        zero_pos();
+        break;
+        default:
+        zero_pos();
+        break;
+    }
+
+}
+
+String Legs::get_current_state_name(){
+    /*
+    Return a String representing the current state.
+    */
+    switch (get_current_state()) {
+        case FORWARD:
+        return "FORWARD";
+        break;
+        case BACKWARD:
+        return "BACKWARD";
+        break;
+        case LEFT:
+        return "LEFT";
+        break;
+        case RIGHT:
+        return "RIGHT";
+        break;
+        case BYELEFT:
+        return "BYELEFT";
+        break;
+        case BYERIGHT:
+        return "BYERIGHT";
+        break;
+        case STOP:
+        return "STOP";
+        break;
+        default:
+        return "STOP";
+        break;
+    }
+}
---
+++ Legs.h
@@ -0,0 +1,200 @@
+/*
+Copyright 2019 Eduardo S. Pereira
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include <Arduino.h>
+#include <Servo.h>
+#include <Wire.h>
+#ifndef LEGS_H
+#define LEGS_H
+
+/*
+                  Front
+             ---------------
+            |     O   O     |
+            |---------------|
+            |               |
+       ----- ---------------  -----
+      | RS1 |                | LS1 |
+       ----                    ----
+  -----                              -----
+ | RI1 |                            | LI1 |
+  -----                              -----
+
+                  Back
+             ---------------
+            |               |
+            |---------------|
+            |               |
+       ----- ---------------  -----
+      | LS2 |                | RS2 |
+       ----                    ----
+ -----                              -----
+| LI2 |                            | RI2 |
+ -----                              -----
+
+
+*/
+
+enum states {FORWARD, BACKWARD, LEFT, RIGHT, BYELEFT, BYERIGHT, STOP, ENDSTATE};
+
+class Legs {
+
+private:
+    Servo servo_rs1;
+    Servo servo_ri1;
+    Servo servo_rs2;
+    Servo servo_ri2;
+    Servo servo_ls1;
+    Servo servo_li1;
+    Servo servo_ls2;
+    Servo servo_li2;
+
+    unsigned long _delay_step = 100;
+    unsigned long _delay_turn = 100;
+    unsigned long _delay_bye = 500;
+    unsigned long t0=millis();
+    int cont_back_for=0;
+    int cont_bye=0;
+    int cont_left_right = 0;
+    int i2c_address = 0x04;
+    float velocity = 0;
+    float distance = -1;
+
+    states current_state = STOP;
+    boolean last_step_movimet = false;
+
+
+    byte _foward[14][8] = { // R2
+                            //rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+                            {110, 10, 30, 170, 30, 100, 160, 30},
+                            {110, 60, 30, 170, 30, 100, 160, 30},
+                            {110, 60, 30, 170, 30, 130, 160, 30},
+                            // R1
+                            //rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+                            {110, 60, 30, 170, 60, 130, 160, 30},
+                            {170, 60, 30, 170, 60, 130, 160, 30},
+                            {170, 60, 30, 170, 30, 130, 160, 30},
+                            //All backward
+                            {110, 10, 30, 170, 30, 130, 160, 30},
+                            //L2
+                            //rs1, rs2,ls1,ls2, ri1, ri2, li1, li2
+                            {110, 10, 30, 170, 30, 130, 160, 60},
+                            {110, 10, 30, 90, 30, 130, 160, 60},
+                            {110, 10, 30, 90, 30, 130, 160, 30},
+                            //L1
+                            //rs1,rs2, ls1, ls2, ri1, ri2, li1, li2
+                            {110, 10, 30, 90, 30, 130, 90, 30},
+                            {110, 10, 0,  90, 30, 130,  90, 30},
+                            {110, 10, 0,  90, 30, 130, 160, 30},
+                            //All backward
+                            {110, 10, 30, 170, 30, 130, 160, 30}
+                            };
+
+    byte _backward[15][8] = {
+                            //rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+                            {120, 30, 30, 120, 30, 130, 130, 30},
+                            //L1
+                            {120, 30, 10, 90, 30, 130, 130, 30},
+                            {120, 30, 0, 90, 30, 130, 90, 30},
+                            {120, 30, 30, 90, 30, 130, 90, 30},
+                            //L2
+                            {120, 30, 30, 90, 30, 130, 130, 30},
+                            {120, 30, 30, 90, 30, 130, 130, 60},
+                            {120, 30, 30, 120, 30, 130, 130, 60},
+                            //All backward
+                            {120, 30, 60, 160, 30, 130, 130, 30},
+                            // R1
+                            {160, 60, 30, 120, 30, 130, 130, 30},
+                            {160, 60, 30, 120, 60, 130, 130, 30},
+                            {120, 60, 30, 120, 60, 130, 130, 30},
+                            // R2
+                            {120, 60, 30, 120, 30, 130, 130, 30},
+                            {120, 60, 30, 120, 30, 100, 130, 30},
+                            {120, 30, 30, 120, 30, 100, 130, 30},
+                            //All backward
+                            {120, 30, 30, 120, 30, 130, 130, 30}
+
+                    };
+
+    byte _left[5][8] = {//rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+                        {120, 30,  30,  120, 30,  130, 130, 30},
+                        {120, 30,  30,  120, 30,  110, 110, 30},
+                        {120, 60,  60,  120, 30,  110, 110, 30},
+                        {120, 60,  60,  120, 60,  130, 130, 60},
+                    };
+
+    byte _right[5][8] = {//rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+                        {120, 30,  30,  120, 30,  130, 130, 30},
+                        {120, 30,  30,  120, 90,  130, 130, 90},
+                        {90,  30,  30,   90, 90,  130, 130, 90},
+                        {90,  30,  30,   90, 30,  110, 110, 30},
+                    };
+
+    byte _bye_left[7][8] = {//rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+                           {120, 30,  30,  120, 30,  130, 130, 30},
+                           {180, 30,  0,   120, 0,   180, 130, 0},
+                           {180, 30,  0,   120, 0,   180,  50, 0},
+                           {180, 30,  0,   120, 0,   180,  90, 0},
+                           {180, 30,  0,   120, 0,   180,  50, 0},
+                           {180, 30,  0,   120, 0,   180,  90, 0},
+                           {120, 30,  30,  120, 30,  130, 130, 30}
+                         };
+
+     byte _bye_right[7][8] = {//rs1, rs2, ls1, ls2, ri1, ri2, li1, li2
+                            {120,  30,  30,   120, 30,  130, 130, 30},
+                            {120,  30,   0,   120, 50,  180, 180, 0},
+                            {120,  30,   0,   120, 90,  180, 180, 0},
+                            {120,  30,   0,   120, 50,  180, 180, 0},
+                            {120,  30,   0,   120, 90,  180, 180, 0},
+                            {120,  30,   0,   120, 50,  180, 180, 0},
+                            {120,  30,  30,   120, 30,  130, 130, 30}
+                          };
+
+public:
+    static Legs * legsi2c;
+    static void receive_data(int byte_count);
+    static void send_data();
+
+    Legs();
+
+    void start_i2c();
+    void set_servos(int rs1, int ri1, int rs2, int ri2,
+                    int ls1, int li1, int ls2, int li2);
+    void zero_pos();
+    void step(int rs1, int rs2, int ls1, int ls2,
+              int ri1, int ri2, int li1, int li2);
+    void move_forward();
+    void move_backward();
+    void turn_left();
+    void turn_right();
+    void bye_bye_left();
+    void bye_bye_right();
+    states get_current_state();
+    String get_current_state_name();
+
+    void set_current_state(states state){current_state = state;}
+    void move_according_state();
+    boolean is_last_step_movimet(){return last_step_movimet;}
+    void set_last_step_movimet(boolean cstep){ last_step_movimet = cstep;}
+    void set_i2c_address(int address){i2c_address = address;}
+    int get_i2c_address(){return i2c_address;}
+    float get_velocity(){return velocity;}
+    float get_distance(){return distance;}
+    void set_velocity(float v){velocity = v;}
+    void set_distance(float d){distance = d;}
+
+};
+
+#endif
